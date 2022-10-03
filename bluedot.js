165ff70fc4c8ff34cb7c00af4af19bbe232f7e90
ardev112 <114944898+ardev112@users.noreply.github.com>
2022-10-03 13:18:12 -0400

    Create bluedot.js

---
+++ bluedot.js
@@ -0,0 +1,23 @@
+const cursor = document.querySelector(".cursor");
+const links = document.querySelectorAll("nav ul li a i");
+const navlinks = document.querySelectorAll("nav ul li i");
+
+document.addEventListener("mousemove", (e) => {
+    let leftPosition = e.pageX + 4;
+    let topPosition = e.pageY + 4;
+
+    cursor.style.left = leftPosition + "px";
+    cursor.style.top = topPosition + "px";
+})
+
+links.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+        cursor.classList.add("large");
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("large");
+    })
+})
